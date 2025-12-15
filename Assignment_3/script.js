830fa7e2c2b6e442d45efbd49052b24ae0e05ac6
Rushikesh4567 <rushikeksh4567@gmail.com>
2025-12-15 23:00:55 +0530

    Assignment 3 Done

---
+++ Assignment_3/script.js
@@ -0,0 +1,12 @@
+function validateLogin(event) {
+    event.preventDefault();
+
+    let password = document.getElementById("password").value;
+
+    if (password.length < 6) {
+        alert("Password must be at least 6 characters");
+        return;
+    }
+
+    alert("Login successful!");
+}
